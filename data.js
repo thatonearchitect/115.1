c46b9576b3072595db132dcd0d2c459b6077c0cf
thatonearchitect <a_bregadze2@cu.edu.ge>
2025-07-01 11:50:58 +0400

    Add files via upload

---
+++ data.js
@@ -0,0 +1,263 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0--living-room",
+      "name": "მისაღები ოთახი/Living room",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 1.0274956201954186,
+          "pitch": 0.237603791303723,
+          "rotation": 0,
+          "target": "5-bathroom"
+        },
+        {
+          "yaw": 1.5148080908212096,
+          "pitch": 0.2332415434256223,
+          "rotation": 0,
+          "target": "4--3bedroom-3"
+        },
+        {
+          "yaw": 1.8473314051313086,
+          "pitch": 0.22359651186155816,
+          "rotation": 0,
+          "target": "3--2bedroom-2"
+        },
+        {
+          "yaw": -1.2064513054814086,
+          "pitch": 0.22251831599844074,
+          "rotation": 0,
+          "target": "1-kitchen"
+        },
+        {
+          "yaw": -2.1498475107532684,
+          "pitch": 0.1479934681059003,
+          "rotation": 0,
+          "target": "2-1bedroom-1"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "1-kitchen",
+      "name": "სამზარეულო/Kitchen",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 1.1446943309043274,
+          "pitch": 0.40445835474333514,
+          "rotation": 0,
+          "target": "0--living-room"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "2-1bedroom-1",
+      "name": "საძინებელი1/Bedroom 1",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 1.493728194630351,
+          "pitch": 0.36945646713633273,
+          "rotation": 0,
+          "target": "0--living-room"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "3--2bedroom-2",
+      "name": "საძინებელი 2/Bedroom 2",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.9199821761609019,
+          "pitch": 0.2954040253646504,
+          "rotation": 0,
+          "target": "0--living-room"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "4--3bedroom-3",
+      "name": "საძინებელი 3/Bedroom 3",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -1.4002495624491065,
+          "pitch": 0.29245934873413404,
+          "rotation": 0,
+          "target": "0--living-room"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "5-bathroom",
+      "name": "სააბაზანო/Bathroom",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 1.8940821223161608,
+          "pitch": 0.5470934830218841,
+          "rotation": 0,
+          "target": "0--living-room"
+        }
+      ],
+      "infoHotspots": []
+    }
+  ],
+  "name": "Project Title",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": false,
+    "fullscreenButton": false,
+    "viewControlButtons": false
+  }
+};
